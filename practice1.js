581c2ea60a2cfae1992c2ee6fd5303bae6e42533
Михаил <108214766+kanekipos2@users.noreply.github.com>
2023-03-03 15:49:23 +0300

    Update practice1.js

--- practice1.js
+++ practice1.js
@@ -13,7 +13,19 @@ const testInput =
 //  Всегда первые два столбца это нумерация и название!
 
 const resolved = tableResolver(testInput);
-const parettoOptimized = paretto(resolved);
+
+console.table(resolved);
+
+//Второй аргумент - столбец, по которому срезаем, начиная с нуля, при этом нельзя использовать значение меньше 2, так так это название и номер
+//Третий - значение, по которому ведётся срез
+var cutted = resolved;
+cutted = borderCut(cutted, 2, 4000);
+cutted = borderCut(cutted, 5, 100);
+cutted = borderCut(cutted, 3, 0.800);
+
+console.table(cutted);
+
+const parettoOptimized = paretto(cutted);
 
 console.table(parettoOptimized);
 console.log(extractNums(parettoOptimized));
@@ -44,7 +56,7 @@ function compare(i1, i2) {
         c2[i] = parseFloat(c2[i]);
         if(c1[i] == c2[i]) eq++;
         else {
-            if(resolved[0][i] == '+') {
+            if(resolved[0][i].charAt(resolved[0][i].length-2) == '+') {
                 if(c1[i] > c2[i]) p++;
                 else p--;
             }
@@ -59,6 +71,21 @@ function compare(i1, i2) {
     return 'н';
 }
 
+function borderCut(table, parametr, value) {
+    const out = [];
+    out.push(table[0]);
+    if(parametr < 2) { console.error('Не может быть параметр 1 или 0!'); return null; }
+    const ch = table[0][parametr].charAt(table[0][parametr].length-2);
+    for(let i = 1; i < table.length; i++) {
+        if(ch == "+") {
+            if(table[i][parametr] >= value) out.push(table[i]);
+        } else {
+            if(table[i][parametr] <= value) out.push(table[i]);
+        }
+    }
+    return out;
+}
+
 function tableResolver(t) {
     t = t.replace(/ /g,'');
     const awns = [];
@@ -67,8 +94,7 @@ function tableResolver(t) {
         if(i!=1) {
             v.split('|').forEach((val, i2) => {
                 if (val) {
-                    if(i == 0 && i2 > 2) tempArr.push(val.slice(-2, -1));
-                    else tempArr.push(val);
+                    tempArr.push(val);
                 }
             })
             awns.push(tempArr)
